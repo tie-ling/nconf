4144d63996f60abfb9470730037b6e2ad64545c2
Yan Cao <yguo@posteo.net>
2024-03-10 08:47:38 +0100

    isync
    Signed-off-by: Yan Cao <yguo@posteo.net>

--- configuration.nix
+++ configuration.nix
@@ -172,29 +172,30 @@
       isNormalUser = true;
       packages = builtins.attrValues {
         inherit (pkgs)
-          mg emacs29-nox mu zathura yt-dlp mpv xournalpp pavucontrol msmtp
+          mg emacs29-nox mu zathura yt-dlp mpv xournalpp pavucontrol msmtp isync
           gpxsee qrencode;
       } ++ [
         (pkgs.pass.withExtensions (exts: [ exts.pass-otp ]))
-        (pkgs.texliveBasic.withPackages (ps: builtins.attrValues {
-          inherit (ps)
-            collection-basic collection-mathscience collection-pictures
-            collection-luatex collection-langenglish collection-langgerman
-            interval
+        (pkgs.texliveBasic.withPackages (ps:
+          builtins.attrValues {
+            inherit (ps)
+              collection-basic collection-mathscience collection-pictures
+              collection-luatex collection-langenglish collection-langgerman
+              interval
 
-            ###### pdf manipulation tool
-            pdfjam # depends on pdfpages, geometry
-            # pdfpages and dependencies
-            pdfpages eso-pic atbegshi pdflscape
-            ######
+              ###### pdf manipulation tool
+              pdfjam # depends on pdfpages, geometry
+              # pdfpages and dependencies
+              pdfpages eso-pic atbegshi pdflscape
+              ######
 
-            # unicode-math and deps
-            unicode-math fontspec realscripts lualatex-math
-            # quotes
-            csquotes
-            # checks
-            chktex lacheck;
-        }))
+              # unicode-math and deps
+              unicode-math fontspec realscripts lualatex-math
+              # quotes
+              csquotes
+              # checks
+              chktex lacheck;
+          }))
       ];
     };
   };
